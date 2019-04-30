c5f57e0bb522e0f13453758020e78b0189f5b345
novandev <donovan.adams@students.makeschool.com>
2019-04-29 23:43:17 -0700

    added analysys

--- ETL-CSV-elasticsearch.py
+++ ETL-CSV-elasticsearch.py
@@ -3,7 +3,7 @@ This file is a basic script that takes the CSV format presented as the following
 
 Ran off on da Plug Twice, Plies, 153878.641, [{"line":"Fuck a shooter I'm my own shooter","milliseconds":"12660","lrc_timestamp":"[00:12.66]"},{"line":"All this ice I'm my own jeweler","milliseconds":"15830","lrc_timestamp":"[00:15.83]"},{"line":"Six lawyers and they all Jewish","milliseconds":"18430","lrc_timestamp":"[00:18.43]"}.......]
 
-and transform it into a json file for uploading
+and transform it into a dataframe
 
 Considerations:
 the line
@@ -14,65 +14,44 @@ import pandas as pd
 
 
 
+# Helper function
+def strip_and_tokenize(string):
+    string = string.split(' ')
+    string = [re.sub('[^0-9a-zA-Z]+', '', s) for s in string]
+    return ' '.join(string)
 
-def open_and_read(filepath):
-    # Open the files as csv_file variable
-    data_df =pd.read_csv(filepath)
-def clean(data):
-    data = list(data)
-    for pos,char in enumerate(data):
-        if data[pos -1] == '"' and char == '"' or data[pos -1] == '"' and char == '[' or data[pos -1] == '"' and char == ']' :
-            del data[pos -1]
-        if  data[pos -1] == ']' and char == '"':
-            del data[pos]   
-            
-        
-    data =  ''.join(data)
-    data = data.strip("").strip("'")
-    # print(data)
-    return data
-def get_lyric_and_indices(str):
-    starts = []
-    stops = []
-    lyrics_list = []
+def cleaner(dataframe,results):
+    just_lyrics = []
+    lyric_info_list = dataframe['lrc'].to_list()
+    # The lyrics are stored in a list that is encasulated in a string, so using an eval() statement
+    # makes sense here if this is expected behavior
+    lyric_info_list= [eval(lyric) for lyric in lyric_info_list]
+
+    # Note that a list compresention with a function could have been used here 
+    for lyrics in lyric_info_list:
+       just_lyrics.append([lyric['line'] for lyric in lyrics])
+    just_lyrics = [' '.join(lyrics).lower() for lyrics in just_lyrics]
+    just_lyrics = [strip_and_tokenize(lyrics) for lyrics in just_lyrics]
+    dataframe = dataframe.join(pd.DataFrame(results))
+    dataframe = dataframe.join(pd.DataFrame(just_lyrics))
+    dataframe.columns = ['title','artist', 'duration','lrc', 'anger', 'fear','joy', 'sadness', 'surprise', 'cleaned_lyrics']
+    dataframe.drop(['lrc'], axis=1,inplace=True)
+    return dataframe
+# print(indicoio.emotion(just_lyrics,top_n='6'))
+
+
+data = cleaner(data_df,results)
+
+def loader(cleaned_dataframe):
     
-    for i in range(len(cleaned_file)):
-        if cleaned_file[i-1] == ',' and cleaned_file[i] == '[':
-            # print(i)
-            starts.append(i) 
-        if cleaned_file[i-1] == '}' and cleaned_file[i]== ']':
-            stops.append(i+1)
-            # print(i) 
-
-    for i in range(len(stops)):
-        lyrics_list.append(cleaned_file[starts[i]:stops[i]].strip())
-
-    # print('{} {}'.format(len(starts), len(stops)))
-    lyrics_list = [eval(x) for x in lyrics_list]
-
-    return(starts,stops,lyrics_list)
-
-def jsonify(cleaned_file):
-    clean_dict = {}
-    # print(cleaned_file)
-    (get_lyric_and_indices(cleaned_file))
-    # print(beginnings)
-
-
-    # print({'title': top_half.split(',')[0], 'artist': top_half.split(',')[1], 'duration':top_half.split(',')[2]})
+    def gendata():
+        for song in cleaned_dataframe:
+            yield {
+                "_index": "songs",
+                "_type": "document",
+                "doc": song,
+            }
     
     
-
-# 4-6
-
-
-# def load(data):
-#     pass
-
-
-if __name__ == "__main__":
-    # file_loaded = open_and_read('./DataSet.csv')
-    print(pd.read_csv('./DataSet.csv'))
-    # cleaned_file = clean(file_loaded)
-    # jsonify(cleaned_file)
     
+    helpers.bulk(es,gendata())
